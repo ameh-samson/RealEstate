01dd45d928189eb9c167b6a806094c9e76321f5b
Samson Ameh <amehsam69@gmail.com>
2024-04-07 19:09:02 +0100

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Hero = () => {
   return (
     <section className="hero-wrapper">
-      <div className="paddings innerWidth flexCenter hero-container"></div>
+      <div className="paddings innerWidth flexCenter hero-container">Hero</div>
     </section>
   );
 };
