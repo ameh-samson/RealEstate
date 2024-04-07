9b287f677ab7f1361160aae1feb711a08b99f999
Samson Ameh <amehsam69@gmail.com>
2024-04-07 18:59:12 +0100

    fork the repo and continue tutorial

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="hero-wrapper">
+      <div className="paddings innerWidth flexCenter hero-container"></div>
+    </section>
+  );
+};
+
+export default Hero;
